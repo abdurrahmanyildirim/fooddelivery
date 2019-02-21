e7b9105aa1a5a815b01606a27d768479a722ecb3
Emre <Emre@EMRE>
2019-02-22 02:55:22 +0300

    UI Tasarımı yapıldı ve görseller eklendi.

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -13,5 +13,15 @@ namespace UI.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
