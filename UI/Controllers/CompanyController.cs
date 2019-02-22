f2c6305ef4743153b68ab48550631855b9efced2
ademcskn <46428223+ademcskn@users.noreply.github.com>
2019-02-23 01:48:31 +0300

    Şirket paneli hazırlandı.

---
+++ UI/Controllers/CompanyController.cs
@@ -0,0 +1,38 @@
+﻿using FoodDelivery.DAL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class CompanyController : Controller
+    {
+        Context db;
+        public CompanyController()
+        {
+            db = new Context();
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        public ActionResult Branches()
+        {
+
+            return View(db.Branches.ToList());
+        }
+        public ActionResult Menus()
+        {
+
+            return View(db.Menus.ToList());
+        }
+        public ActionResult AddBranch()
+        {
+            ViewBag.Cities = db.Cities.ToList();
+            ViewBag.Regions = db.Regions.ToList();
+            return View();
+        }
+    }
+}
