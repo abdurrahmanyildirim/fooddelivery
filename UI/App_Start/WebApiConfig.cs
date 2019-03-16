cb5b09443ad2b3227da7449aa7c4adda9c356050
2Lightning2 <43090396+2Lightning2@users.noreply.github.com>
2019-03-16 16:46:31 +0300

    ApiExecptionAttribute implementasyonu yapıldı.

--- UI/App_Start/WebApiConfig.cs
+++ UI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using UI.Attributes;
 
 namespace UI
 {
@@ -10,6 +11,7 @@ namespace UI
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+            config.Filters.Add(new ApiExecptionAttribute());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
